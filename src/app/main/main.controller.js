8c480e79efb4a73294d0d9b4115300b25c71f968
Louis Wilbrink <louis.wilbrink@gmail.com>
2015-02-17 21:38:45 -0500

    added addTime functionality -- start and stop saved in total minutes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,11 +9,43 @@ angular.module('contractTimer')
         displayCompany, 
         Company) {
 
+        function convertToMinutes (hours, minutes) {
+            
+            hours = parseInt(hours);
+            minutes = parseInt(minutes);
+
+            return (hours * 60) + minutes;
+        }
+
         $scope.displayCompany = displayCompany;
 
         $scope.workshifts = allWorkshifts;
 
         $scope.showAllCompanies = false;
+
+        $scope.addTime = function () {
+
+            var start, stop, timeSegments;
+
+            timeSegments = $scope.start.split(':');
+            start = convertToMinutes(timeSegments[0], timeSegments[1]);
+
+            timeSegments = $scope.stop.split(':');
+            stop = convertToMinutes(timeSegments[0], timeSegments[1]);
+
+            var uniqueShift = $scope.displayCompany + Date.now().toString();
+
+            $scope.workshifts[uniqueShift] = {
+                company: $scope.displayCompany,
+                start: start,
+                stop: stop
+            };
+            
+            $scope.workshifts.$save().then(function () {
+                // Add toast.
+                console.log('saved!');
+            });
+        };
 
         $scope.$on('displayCompanySelected', function (event, displayCompany) {
             $scope.displayCompany = displayCompany;
